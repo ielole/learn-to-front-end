96b90dc48438385ef17f3b1815da9803607f80b8
ielole <ieloletin@gmail.com>
2017-01-15 10:50:17 -0500

    Add delete goal functionality.
    - Add a delete button to goals-main template.hbs.
    - Add an action to the delete button.
    - Add delete action to goals-main component.js.
    - Add delete/deleteList action to goals template.hbs.
    - Add deleteList action to goals route.js.

--- app/components/goals-main/component.js
+++ app/components/goals-main/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete () {
+      console.log("I'm in the goals-main component and I am deleting this goal: ", this.get('goal'));
+      this.sendAction('delete', this.get('goal'));
+    },
+  },
 });
